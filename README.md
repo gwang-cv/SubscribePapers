fa63a0f2e105e053ad08e659f8305613026ebbf4
gwang.cv <gwang.cv@gmail.com>
2018-10-29 00:37:06 +0800

    build

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Subscribe New Papers
 
+[![Build Status](https://travis-ci.com/gwang-cv/SubscribePapers.svg?branch=master)](https://travis-ci.com/gwang-cv/SubscribePapers)
 
 Subscribe new papers from arxiv (CS) and send paper list via EMail daily.
+
+Travis CI is used to build the repository and send EMail. 
+
+PS: You need to build the code successfully via Travis.
+
--- core.py
+++ core.py
@@ -53,85 +53,17 @@ def get_email_content():
         if (indx % 2) == 0:
             paper=paper+'<tr><td>{0}</td><td><a href="{1}">{2}</td></tr>'.format(indx+1,href, title)
         else:
-            paper=paper+'<tr><td class="alt">{0}</td><td class="alt"><a href="{1}">{2}</td></tr>'.format(indx+1,href, title)
+            paper=paper+'<tr><td background: #F5FAFA; color: #797268;>{0}</td><td background: #F5FAFA; color: #797268;><a href="{1}">{2}</td></tr>'.format(indx+1,href, title)
 
-    html = """
-        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-        <html xmlns="http://www.w3.org/1999/xhtml">
-        <head>
-        {0}
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>CSS Tables</title>
-        <link href="styles.css" rel="stylesheet" type="text/css" />
-        </head>
-        <style type="text/css">
-
-        body {
-        font: normal 11px auto "Trebuchet MS", Verdana, Arial, Helvetica, sans-serif;
-         color: #4f6b72;
-         background: #E6EAE9;
-        }
-
-        a {
-         color: #c75f3e;
-        }
-        caption {
-         padding: 0 0 5px 0;
-         width: 700px;  
-         font: italic 11px "Trebuchet MS", Verdana, Arial, Helvetica, sans-serif;
-         text-align: right;
-        }
-        th {
-         font: bold 11px "Trebuchet MS", Verdana, Arial, Helvetica, sans-serif;
-         color: #4f6b72;
-         border-right: 1px solid #C1DAD7;
-         border-bottom: 1px solid #C1DAD7;
-         border-top: 1px solid #C1DAD7;
-         letter-spacing: 2px;
-         text-transform: uppercase;
-         text-align: left;
-         padding: 6px 6px 6px 12px;
-         background: #CAE8EA url(images/bg_header.jpg) no-repeat;
-        }
-        th.nobg {
-         border-top: 0;
-         border-left: 0;
-         border-right: 1px solid #C1DAD7;
-         background: none;
-        }
-        td {
-         border-right: 1px solid #C1DAD7;
-         border-bottom: 1px solid #C1DAD7;
-         background: #fff;
-         font-size:11px;
-         padding: 6px 6px 6px 12px;
-         color: #4f6b72;
-        }
-        td.alt {
-         background: #F5FAFA;
-         color: #797268;
-        }
-        th.spec {
-         border-left: 1px solid #C1DAD7;
-         border-top: 0;
-         background: #fff url(images/bullet1.gif) no-repeat;
-         font: bold 10px "Trebuchet MS", Verdana, Arial, Helvetica, sans-serif;
-        }
-        th.specalt {
-         border-left: 1px solid #C1DAD7;
-         border-top: 0;
-         background: #f5fafa url(images/bullet2.gif) no-repeat;
-         font: bold 10px "Trebuchet MS", Verdana, Arial, Helvetica, sans-serif;
-         color: #797268;
-        }
-        </style>
+    html = """      
+     <html>
+        <head> {0} </hdead>
         <body>
         <table cellspacing="0" >
                 {1}
-                </table>
+        </table>
         </body>
-        </html>
-    """
+    </html>"""
     return html.format(folder[1],paper)
 
 
